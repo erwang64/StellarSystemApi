3cf142b569ddb026206a063dabe7af9f544ceb13
Erwang64 <156552946+erwang64@users.noreply.github.com>
2026-03-31 10:19:30 +0200

    feat(security): Implement UserService

---
+++ src/main/java/edu/esiea/stellarsystemapi/security/services/UserService.java
@@ -0,0 +1,48 @@
+package edu.esiea.stellarsystemapi.security.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.esiea.stellarsystemapi.security.model.User;
+import edu.esiea.stellarsystemapi.security.repositories.UserRepository;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+
+    public Optional<User> getUserById(int id) {
+        return userRepository.findById(id);
+    }
+
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+
+
+    public void deleteUser(int id) {
+        userRepository.deleteById(id);
+    }
+
+
+    public Optional<User> findByLogin(String login) {
+        return userRepository.findByLogin(login);
+    }
+}
